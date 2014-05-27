0356675d406904005109682ea405109e4e65bb77
Patrick McElwee <pmm21@duke.edu>
2014-05-27 09:45:01 -0400

    Working through Hello World tutorial

--- desktop/src-common/our_first_game/core.clj
+++ desktop/src-common/our_first_game/core.clj
@@ -1,17 +1,34 @@
 (ns our-first-game.core
   (:require [play-clj.core :refer :all]
-            [play-clj.ui :refer :all]))
+            [play-clj.g2d :refer :all]))
 
 (defscreen main-screen
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage))
-    (label "Hello world!" (color :white)))
+    (assoc (texture "clojure.svg.png")
+           :x 50 :y 50 :width 100 :height 100
+           :angle 45 :origin-x 0 :origin-y 0
+           ))
   
   :on-render
   (fn [screen entities]
     (clear!)
-    (render! screen entities)))
+    (render! screen entities))
+  :on-key-down
+  (fn [screen entities]
+    (cond
+      (= (:key screen) (key-code :dpad-up))
+      (println "up")
+      (= (:key screen) (key-code :dpad-down))
+      (println "down")
+      (= (:key screen) (key-code :dpad-right))
+      (println "right")
+      (= (:key screen) (key-code :dpad-left))
+      (println "left")
+      )
+    )
+  )
 
 (defgame our-first-game
   :on-create
