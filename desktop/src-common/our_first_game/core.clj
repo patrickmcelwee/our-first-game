9664ccab1b73ab72381022ed3a8e4f7a787bfbb9
Patrick McElwee <pmm21@duke.edu>
2014-05-27 20:59:14 -0400

    We can move Finn's guy around!

--- desktop/src-common/our_first_game/core.clj
+++ desktop/src-common/our_first_game/core.clj
@@ -2,13 +2,22 @@
   (:require [play-clj.core :refer :all]
             [play-clj.g2d :refer :all]))
 
+(def speed 10)
+
+(defn move [entity direction]
+  (case direction
+    :down (assoc entity :y (- (:y entity) speed))
+    :up (assoc entity :y (+ (:y entity) speed))
+    :right (assoc entity :x (+ (:x entity) speed))
+    :left (assoc entity :x (- (:x entity) speed))
+    nil))
+
 (defscreen main-screen
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage))
-    (assoc (texture "clojure.svg.png")
-           :x 50 :y 50 :width 100 :height 100
-           :angle 45 :origin-x 0 :origin-y 0
+    (assoc (texture "game_guy1.png")
+           :x 50 :y 50 :width 186 :height 431
            ))
   
   :on-render
@@ -19,14 +28,28 @@
   (fn [screen entities]
     (cond
       (= (:key screen) (key-code :dpad-up))
-      (println "up")
+      (move (first entities) :up)
       (= (:key screen) (key-code :dpad-down))
-      (println "down")
+      (move (first entities) :down)
       (= (:key screen) (key-code :dpad-right))
-      (println "right")
+      (move (first entities) :right)
       (= (:key screen) (key-code :dpad-left))
-      (println "left")
+      (move (first entities) :left)
       )
+    )
+  :on-touch-down
+  (fn [screen entities]
+    (let [position (input->screen screen (:input-x screen) (:input-y screen))]
+      (cond
+        (> (:y position) (* (height screen) (/ 2 3)))
+        (move (first entities) :up)
+        (< (:y position) (/ (height screen) 3))
+        (move (first entities) :down)
+        (> (:x position) (* (width screen) (/ 2 3)))
+        (move (first entities) :right)
+        (< (:x position) (/ (width screen) 3))
+        (move (first entities) :left)
+        ))
     )
   )
 
