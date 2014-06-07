90a7cedeca1847ebf70f64df96b35f9a8865ef8c
Patrick McElwee <pmm21@duke.edu>
2014-06-07 15:21:34 -0400

    Press 'h' to return hero home

--- desktop/src-common/our_first_game/core.clj
+++ desktop/src-common/our_first_game/core.clj
@@ -2,7 +2,7 @@
   (:require [play-clj.core :refer :all]
             [play-clj.g2d :refer :all]))
 
-(def speed 10)
+(def speed 30)
 
 (defn move [entity direction]
   (case direction
@@ -15,27 +15,33 @@
 (defscreen main-screen
   :on-show
   (fn [screen entities]
-    (update! screen :renderer (stage))
+    (update! screen :renderer (stage) :camera (orthographic))
     (assoc (texture "game_guy1.png")
-           :x 50 :y 50 :width 186 :height 431
+           :x 50 :y 50 :width 93 :height 215 :direction :right
            ))
   
   :on-render
   (fn [screen entities]
     (clear!)
     (render! screen entities))
+  :on-resize
+  (fn [screen entities]
+    (height! screen 600))
   :on-key-down
   (fn [screen entities]
-    (cond
-      (= (:key screen) (key-code :dpad-up))
-      (move (first entities) :up)
-      (= (:key screen) (key-code :dpad-down))
-      (move (first entities) :down)
-      (= (:key screen) (key-code :dpad-right))
-      (move (first entities) :right)
-      (= (:key screen) (key-code :dpad-left))
-      (move (first entities) :left)
-      )
+    (let [hero (first entities)]
+      (cond
+        (= (:key screen) (key-code :h))
+        (assoc hero :x 50 :y 50)
+        (= (:key screen) (key-code :dpad-up))
+        (move hero :up)
+        (= (:key screen) (key-code :dpad-down))
+        (move hero :down)
+        (= (:key screen) (key-code :dpad-right))
+        (move hero :right)
+        (= (:key screen) (key-code :dpad-left))
+        (move hero :left)
+      ))
     )
   :on-touch-down
   (fn [screen entities]
